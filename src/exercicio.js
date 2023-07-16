f650d8cd0ccee3163a0c5325d2e44173bae1e92e
gleison <gleisondeivison3@gmail.com>
2023-07-16 10:48:59 -0300

    tarefaM26

---
+++ src/exercicio.js
@@ -0,0 +1,15 @@
+"use strict";
+let a = 3;
+let b = 2;
+let c = 6;
+function multiplicar(a, b, c) {
+    console.info("O resultado é = "
+        + a * b * c + " ");
+}
+multiplicar(a, b, c);
+let y = "Olá ";
+let x = "Ana";
+function concatenar(y, x) {
+    console.info("saudação _ " + y + x);
+}
+concatenar(y, x);
---
+++ src/exercicio.ts
@@ -0,0 +1,20 @@
+let a: number = 3;
+let b: number = 2;
+let c: number = 6;
+
+function multiplicar(a:number, b:number, c:number): void {
+    console.info("O resultado é = "
+    + a * b * c + " ");
+}
+
+multiplicar(a, b, c);
+
+let y = "Olá ";
+let x = "Ana";
+
+function concatenar(y: string, x: string): any {
+    console.info
+    ("saudação _ " + y + x );
+}
+
+concatenar(y, x);
